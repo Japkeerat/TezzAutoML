2a42fac6dea922a2c61682cbf15e842e54aaea9f
japkeerat <japkeerat21@gmail.com>
2023-11-10 23:10:21 +0530

    CI/CD Functionality

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ with open('requirements.txt') as f:
 
 setup(
     name="TezzAutoML",
-    version="0.1.0",
-    description="An AutoML library built on top of Optuna.",
+    version="0.1.2",
+    description="Just another AutoML library, but better and faster.",
     author="Japkeerat Singh",
     author_email="japkeerat21@gmail.com",
     packages=find_packages(),
